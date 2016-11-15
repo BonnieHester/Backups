7254bf7690ada24eefca0ce558ed6e892a8570df
BonnieHester <bhester@uga.edu>
2016-11-15 11:30:44 -0500

    lacks comments

---
+++ NEWrepl.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <string>
+#include "myProcess.h"
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    //declaration of variables
+    int numPipes = 0;
+    bool printError = false;
+    string input = "";
+    string jobIn = "STDIN_FILENO";
+    string jobOut = "STDOUT_FILENO";
+    string jobErr = "STDERR_FILENO";
+
+    vector<myProcess> allProcesses;
+    vector<myProcess>::iterator allProcessesIter;
+    vector<string> listArguments;
+
+    //continues accepting lines of input until the user enters quit or logout
+    while ((input.compare("quit")) != 0 && (input.compare("logout")) != 0) {
+
+        allProcesses.clear();
+        listArguments.clear();
+        jobIn = "STDIN_FILENO";
+        jobOut = "STDOUT_FILENO";
+        jobErr = "STDERR_FILENO";
+        numPipes = 0;
+        printError = false;
+        cout << "\nrepl$ ";
+        getline(cin, input);
+
+        if (input.compare("quit") == 0 || (input.compare("logout")) == 0)
+            return EXIT_SUCCESS;
+
+        //declaration of variables used in char-iterating loop to build each myProcess object
+        string commandName = "";
+        const char * inputChars = input.c_str();
+        bool sameCommand = true;
+
+        for (int i = 0; i < input.length();) {
+            sameCommand = true;
+            if (inputChars[i] != '|' && inputChars[i] != '"') {
+                while (inputChars[i] == ' ')
+                    i++;
+                size_t sizeWord = input.find(" ", i);
+                if (sizeWord == string::npos)
+                    sizeWord = input.length();
+                commandName = input.substr(i, (sizeWord - i));
+                int temp = i;
+                i += (sizeWord - temp);
+                while (sameCommand && inputChars[i] != '\0') {
+                    while (inputChars[i] == ' ')
+                        i++;
+                    if (inputChars[i] != '|' && inputChars[i] != '"') {
+
+                        if (input.find("< ", i) == i) {
+                            i += 2;
+                            sizeWord = input.find(" ", i);
+                            if (sizeWord == string::npos)
+                                sizeWord = input.length();
+                            string arg = (input.substr(i, (sizeWord - i)));
+                            jobIn = arg;
+                            int tempI = i;
+                            i += (sizeWord - tempI);
+                        }
+                        else if (input.find("e> ", i) == i) {
+                            i += 3;
+                            sizeWord = input.find(" ", i);
+                            if (sizeWord == string::npos)
+                                sizeWord = input.length();
+                            string arg = (input.substr(i, (sizeWord - i)));
+                            jobErr = arg;
+                            jobErr += " (truncate)";
+                            int tempI = i;
+                            i += (sizeWord - tempI);
+                        }
+                        else if (input.find("e>> ", i) == i) {
+                            i += 4;
+                            sizeWord = input.find(" ", i);
+                            if (sizeWord == string::npos)
+                                sizeWord = input.length();
+                            string arg = (input.substr(i, (sizeWord - i)));
+                            jobErr = arg;
+                            jobErr += " (append)";
+                            int tempI = i;
+                            i += (sizeWord - tempI);
+                        }
+                        else if (input.find("> ", i) == i) {
+                            i += 2;
+                            sizeWord = input.find(" ", i);
+                            if (sizeWord == string::npos)
+                                sizeWord = input.length();
+                            string arg = (input.substr(i, (sizeWord - i)));
+                            jobOut = arg;
+                            jobOut += " (truncate)";
+                            int tempI = i;
+                            i += (sizeWord - tempI);
+                        }
+                        else if (input.find(">> ", i) == i) {
+                            i += 3;
+                            sizeWord = input.find(" ", i);
+                            if (sizeWord == string::npos)
+                                sizeWord = input.length();
+                            string arg = (input.substr(i, (sizeWord - i)));
+                            jobOut = arg;
+                            jobOut += " (append)";
+                            int tempI = i;
+                            i += (sizeWord - tempI);
+                        }
+                        else {
+                            sizeWord = input.find(" ", i);
+                            if (sizeWord == string::npos)
+                                sizeWord = input.length();
+                            string argName = (input.substr(i, (sizeWord - i)));
+                            listArguments.push_back(argName);
+                            int tempI = i;
+                            i += (sizeWord - tempI);
+                        }
+                    }
+                    else if (inputChars[i] == '"') { //needs to be changed to account for uneven quote number error
+                        bool inQuote = true;
+                        string tempName = "";
+                        i++;
+                        while (inQuote) {
+                            if (inputChars[i] == '\0') {
+                                cout << "Error - expression after \" never completed. Please try again." << endl;
+                                printError = true;
+                                inQuote = false;
+                            }
+                            if (inputChars[i] != '"') {
+                                if (inputChars[i] != '\\')
+                                    tempName += inputChars[i];
+                            }
+                            else {
+                                if (inputChars[i-1] == '\\')
+                                    tempName += inputChars[i];
+                                else
+                                    inQuote = false;
+                            }
+                            i++;
+                        }
+                        i++;
+                        listArguments.push_back(tempName);
+                    }
+                    else if (inputChars[i] == '|') {
+                        myProcess newProcess(commandName, listArguments, (listArguments.size() + 1));
+                        allProcesses.push_back(newProcess);
+                        i++;
+                        numPipes++;
+                        sameCommand = false;
+                        listArguments.clear();
+                    }
+                }
+            }
+            else {
+                cout << "Error: invalid input - cannot lead off with '|' or '\"'. Please try again." << endl;
+                printError = true;
+                break;
+            }
+        }
+        //creating and adding to vector any process not followed by a |
+        myProcess newProcess(commandName, listArguments, (listArguments.size() + 1));
+        allProcesses.push_back(newProcess);
+
+        if (!printError) {
+            cout << "\nJob STDIN  = " << jobIn << endl;
+            cout << "Job STDOUT = " << jobOut << endl;
+            cout << "Job STDERR = " << jobErr << endl;
+
+            cout << '\n' << numPipes << " pipe(s)" << endl;
+            cout << allProcesses.size() << " process(es)" << endl;
+
+            int counter = 0;
+
+            //use iter to iterate through and print info about each process
+            for (allProcessesIter = allProcesses.begin(); allProcessesIter != allProcesses.end(); allProcessesIter++) {
+                cout << "\nProcess " << counter << " argv:" << endl;
+                counter++;
+                myProcess tempProcess = *allProcessesIter;
+
+                vector<string> tempList = tempProcess.getListOfArgs();
+                vector<string>::iterator tempListIter;
+                string nameOfCommand = tempProcess.getCommand();
+
+                cout << "0: " << nameOfCommand << endl;
+                int listCounter = 1;
+
+                for (tempListIter = tempList.begin(); tempListIter != tempList.end(); tempListIter++) {
+                    cout << listCounter << ": " << *tempListIter << endl;
+                    listCounter++;
+                }
+            }
+        }
+    }
+}
